71a6d9af071fabe530fbcafa295ce1a2a310283f
shayan ali <plzmujeychordo633@gmail.com>
2026-01-02 14:45:37 +0500

    Use env-based API base URL

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_SERVER as string;
+
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: BASE_URL,
   withCredentials: true,
 });
 
